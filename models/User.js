0223ade6312e056647290759c0d12a9568dc35a8
Deena Smith <deenasmith1630@gmail.com>
2022-06-09 11:33:54 -0500

    User model built, no validation match yet

---
+++ models/User.js
@@ -0,0 +1,48 @@
+
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: string,
+            unique: true,
+            required: 'Enter your username.',
+            trim: true
+        },
+        email: {
+            type: string, 
+            required: 'Enter your email.',
+            unique: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+
+module.exports = User;
